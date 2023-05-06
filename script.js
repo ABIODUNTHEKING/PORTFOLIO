23b2ec825afcde76656997f1931e1ac611489fd8
Emmanuel Abiodun Akinjogunla <105178231+ABIODUNTHEKING@users.noreply.github.com>
2023-05-06 21:19:06 +0100

    DONE

--- script.js
+++ script.js
@@ -14,30 +14,19 @@ let listedTools = document.getElementById('listedTools')
 let projects = document.getElementById('projects')
 let contactForm = document.getElementById('contactForm')
 let seeMoreButton = document.getElementById('seeMore')
-
-
-toggleButton.addEventListener("click", ()=>{
-    if(toggleButton.innerHTML == `<i class="fa-regular fa-sun"></i>`){
-    toggleButton.innerHTML = `<i class="fa-regular fa-moon"></i>`
-    toggleButton.style.backgroundColor = "rgb(30,48,80)"
-    body.style.backgroundColor = "white"
-    logo.setAttribute('src', '/image/black logo.svg')
-    body.style.color = "white"
-    intro.classList.add("toggledIntro")
-    tools.classList.add("toggledTools")
-    listedTools.classList.add("toggledListedTools")
-    title.classList.add("toggledtitle")
-    projects.classList.add("toggledProject")
-    contactForm.classList.add("toggledContactForm")
-    navBar.classList.add('toggledNavBar')
-    navBarTwo.classList.add('toggledNavBar')
-    seeMoreButton.classList.add('toggledButton')
-    }
-    else{
+let mode = localStorage.getItem('mode') 
+changeAppMode(mode)
+          
+function changeAppMode(mode){
+    let backgroundColor
+    if(mode == 'dark'){
+        backgroundColor = "rgba(2, 0, 0, 0.699)"
         toggleButton.innerHTML = `<i class="fa-regular fa-sun"></i>`
-        toggleButton.style.backgroundColor = "yellow"
-        body.style.backgroundColor = "rgba(2, 0, 0, 0.699)"
+        toggleButton.style.backgroundColor = "yellow" 
+        
+        body.style.backgroundColor = backgroundColor
         logo.setAttribute('src', '/image/white logo.svg')
+        body.style.color = "white"
         intro.classList.remove("toggledIntro")
         tools.classList.remove("toggledTools")
         listedTools.classList.remove("toggledListedTools")
@@ -47,7 +36,34 @@ toggleButton.addEventListener("click", ()=>{
         navBar.classList.remove('toggledNavBar')
         navBarTwo.classList.remove('toggledNavBar')
         seeMoreButton.classList.remove('toggledButton')
+       
     }
+    else{
+        backgroundColor = 'rgba(255, 255, 255, 0.712)'
+        toggleButton.innerHTML = `<i class="fa-regular fa-moon"></i>`
+        toggleButton.style.backgroundColor = 'rgb(30,48,80)'
+        body.style.backgroundColor = backgroundColor
+        logo.setAttribute('src', '/image/black logo.svg')
+        intro.classList.add("toggledIntro")
+        tools.classList.add("toggledTools")
+        listedTools.classList.add("toggledListedTools")
+        title.classList.add("toggledtitle")
+        projects.classList.add("toggledProject")
+        contactForm.classList.add("toggledContactForm")
+        navBar.classList.add('toggledNavBar')
+        navBarTwo.classList.add('toggledNavBar')
+        seeMoreButton.classList.add('toggledButton')
+        
+    }
+} 
+
+
+toggleButton.addEventListener("click", ()=>{
+    let mode = localStorage.getItem('mode')
+    let currentMode = mode == 'dark' ? 'light' : 'dark'
+    changeAppMode(currentMode)
+    localStorage.setItem('mode', currentMode)
+   
 })
 
 
@@ -67,3 +83,6 @@ window.addEventListener("scroll", ()=>{
 
 
 })
+
+
+
